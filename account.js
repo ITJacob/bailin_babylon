2753578d5415718b933599e471e5a8fd07483fa0
Dong Wang <jacob.wd8816@gmail.com>
2024-11-19 10:37:46 +0800

    Update account.js

--- account.js
+++ account.js
@@ -8,14 +8,15 @@ let global = {
 // 测试用
 window.GOBE.Logger.level = window.GOBE.LogLevel.INFO;
 
-const [openId, roomId] = window.location.hash.replace('#', '').split('_');
+const openId = window.location.hash.replace("#", "");
 
 // 实例化Client对象
 global.client = new window.GOBE.Client({
   appId: "245150415728004841", // 应用ID，具体获取可参见准备游戏信息
   openId, // 玩家ID，区别不同用户
   clientId: "1557244579667799872", // 客户端ID，具体获取可参见准备游戏信息
-  clientSecret: "C017C61E129E83650D935387B6D1253107A36E197EAB668FCA7D89E5DF1D5B5A", // 客户端密钥，具体获取可参见准备游戏信息
+  clientSecret:
+    "C017C61E129E83650D935387B6D1253107A36E197EAB668FCA7D89E5DF1D5B5A", // 客户端密钥，具体获取可参见准备游戏信息
   platform: { platform: GOBE.PlatformType.WEB }, // 平台类型（选填）
 });
 
@@ -32,43 +33,37 @@ async function start() {
 
   // 2 获取房间列表
   // 查询房间列表成功
-  // let { rooms } = await global.client.getAvailableRooms({ limit: 10 });
+  let { rooms } = await global.client.getAvailableRooms({ limit: 10 });
 
-  let room; // 房主信息
   const props = {
     customPlayerStatus: 0,
     customPlayerProperties: "",
   };
-  // 3 加入房间
-  if (roomId) {
-    // 加入房间中
-    room = await global.client.joinRoom(roomId, props);    
-  } else {
-    // 没有房间，自己成为房主
-    room = await global.client.createRoom({ maxPlayers: 2 }, props);
+  
+  // 3 创建房间
+  if (!rooms || rooms.length === 0) {
+    const room = await global.client.createRoom({ maxPlayers: 2 }, props);
     alert(room.roomId);
-  }
-
-  global.room = room;
-  global.player = room.player; // 玩家自己
-
-  // 4 房主准备开始
-  if (playerId === global.room.ownerId) {
+    global.room = room;
+    global.player = room.player; // 玩家自己
     global.room.onJoin((playerInfo) => {
       // 判断是否非房主玩家加入房间
       if (playerInfo.playerId !== global.room.ownerId) {
         global.room.startFrameSync();
+        console.log('start frame!!!!!!!')
       }
     });
+    return {};
   }
 
-  // return new Promise((res) => {
-  //   // 添加帧同步开始通知回调
-  //   global.room.onStartFrameSync(() => {
-  //     // 接收帧同步开始通知，处理游戏逻辑
-  //     res();
-  //   });
-  // });
+  return {
+    rooms,
+    join: async (roomCode) => {
+      const room = await global.client.joinRoom(roomCode, props);
+      global.room = room;
+      global.player = room.player; // 玩家自己
+    }
+  }
 }
 
 let playerId = "";
